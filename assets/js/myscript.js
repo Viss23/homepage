f1dad2642545ea8c5bf268c0021bd97b135dd285
viss23 <wissper777@gmail.com>
2020-05-03 19:39:29 +0300

    Likes Dislikes
    Added feature with background change and ghost

--- assets/js/myscript.js
+++ assets/js/myscript.js
@@ -1,8 +1,24 @@
-window.onload =function (){
+function switchToColor(color) {
+  try {
+    document.querySelectorAll(".page").forEach(function(page) {
+      page.style.borderColor = color;
+    });
+    document.querySelectorAll(".avatar").forEach(function(avatar) {
+      avatar.contentDocument.getElementById("circle").setAttribute("fill", color);
+    });
+  } catch (error) {}
+}
 
+window.onload =function (){
+  document.getElementById("blood").addEventListener("click", switchToColor.bind(this, "#CC1100"));
+  document.getElementById("red").addEventListener("click", switchToColor.bind(this, "red"));
   document.getElementById("speech").addEventListener("click", function() {
   this.childNodes[2].play();
 });
-  
+  this.document.getElementById("ghost").addEventListener("click",function(){
+    document.getElementById("ghost-animation").style.display="block"
+    setTimeout((elem)=>{
+      document.getElementById("ghost-animation").style.display="none"
+    },5700)
+  })
 }
-
